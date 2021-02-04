2042ab9a301e91b40fb63a0bb6f2af1a56ae4a3e
pavyer <pavyer@pavyer.com>
2021-02-04 18:24:54 +0530

    Simpler algorithm for places in number

--- chapter3.js
+++ chapter3.js
@@ -1,8 +1,8 @@
-let inputNumber = 4936;
+/* let inputNumber = 4936;
 
 let onesDigit = inputNumber % 10;
 
-let tensNumber = (inputNumber - onesDigit) % 100;
+let tensNumber = (inputNumber - onesDigit) % 100; // aahh was doing modulus 100 instead of thinking in terms of modulus 10
 
 let  tensDigit = tensNumber / 10;
 
@@ -16,4 +16,26 @@ console.log(`Thousands digit is ${thousandsDigit}`);
 console.log(`Hundreds digit is ${hundredsDigit}`);
 console.log(`Tens digit is ${tensDigit}`);
 console.log(`Ones digit is ${onesDigit}`);
+*/
 
+// Hmm, the easier algorithm is this. What's presented in the solution. Just 
+// start dividing by 10 and taking the modulus. 
+
+let number = 4936;
+
+let ones = number % 10;
+
+number = (number - ones) / 10; // don't have to preserve the original number anymore
+
+let tens = number % 10;
+
+number = (number - tens) / 10;
+
+let hundreds = number % 10;
+
+let thousands = (number - hundreds) / 10;
+
+console.log(`Thousands digit is ${thousands}`);
+console.log(`Hundreds digit is ${hundreds}`);
+console.log(`Tens digit is ${tens}`);
+console.log(`Ones digit is ${ones}`);
