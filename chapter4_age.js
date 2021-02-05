5658ade14bd50cea719ecc3f9ee2e4f4503b30ec
pavyer <pavyer@pavyer.com>
2021-02-05 18:31:06 +0530

    Problems from chapter 4

---
+++ chapter4_age.js
@@ -0,0 +1,7 @@
+let userAge = 20;
+
+console.log("You are 20 years old.");
+console.log(`In 10 years you'll be ${userAge + 10} old.`);
+console.log(`In 20 years you'll be ${userAge + 20} old.`);
+console.log(`In 30 years you'll be ${userAge + 30} old.`);
+console.log(`In 40 years you'll be ${userAge + 40} old.`);
---
+++ chapter4_greeter.js
@@ -0,0 +1,5 @@
+let inputName = "Victor";
+
+console.log(`Good Morning, ${inputName}.`);
+console.log(`Good Afternoon, ${inputName}.`);
+console.log(`Good Evening, ${inputName}.`);
